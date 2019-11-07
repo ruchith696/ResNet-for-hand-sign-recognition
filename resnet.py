e5848a87f32c2a6dfab7f3bb69015187313ac5bb
ruchith696 <34888753+ruchith696@users.noreply.github.com>
2019-11-07 11:26:43 +0530

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -6,15 +6,14 @@ Created on Wed Nov  6 17:51:30 2019
 """
 
 from cnn_utils import *
-
 from blocks import *
 
 import keras.backend as K
 K.set_image_data_format('channels_last')
 K.set_learning_phase(1)
 
-model = ResNet50(input_shape = (64, 64, 3), classes = 6)
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+#model = ResNet50(input_shape = (64, 64, 3), classes = 6)
+#model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
 
 # Normalize image vectors
@@ -32,10 +31,10 @@ print ("Y_train shape: " + str(Y_train.shape))
 print ("X_test shape: " + str(X_test.shape))
 print ("Y_test shape: " + str(Y_test.shape))
 
-#model.load_weights('ResNet50.h5')
-model.fit(X_train, Y_train, epochs = 15, batch_size = 32)
+model.load_weights('ResNet50.h5')
+#model.fit(X_train, Y_train, epochs = 15, batch_size = 32)
 
-model.save_weights('ResNet50.h5')
+#model.save_weights('ResNet50.h5')
 
 preds = model.evaluate(X_test, Y_test)
 print ("Loss = " + str(preds[0]))
